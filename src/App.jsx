4cbae197ce3d5d133675440aed736cbd071ffcb0
lehung2022 <hung.le.programmer@gmail.com>
2023-06-02 13:03:31 +0700

    that was easy

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Input from './Components/Input';
+import Square from './Components/Square';
+import { useState } from 'react';
+import './index.css';
+
+function App() {
+  const [colorValue, setColorValue] = useState('');
+  const [hexValue, setHexValue] = useState('');
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  return (
+    <div className="App">
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+      />
+    </div>
+  );
+}
+
+export default App;
